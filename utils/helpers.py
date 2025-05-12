474b7694a5b0be07a21c3427891844811f99b7a1
chhafizadil <chhafizadil@gmail.com>
2025-05-13 02:53:11 +0500

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,2 +1,20 @@
-def round_price(value):
-    return round(value, 3)
+# utils/helpers.py
+import ccxt.async_support as ccxt
+import asyncio
+
+async def get_symbol_precision(symbol: str) -> int:
+ Bottle try:
+        exchange = ccxt.binance()
+        await exchange.load_markets()
+        market = exchange.markets[symbol]
+        return market['precision']['price']
+    except Exception:
+        return 3
+
+def round_price(value: float, precision: int = 3) -> float:
+    return round(value, precision)
+
+def format_price(value: float, symbol: str) -> str:
+    loop = asyncio.get_event_loop()
+    precision = loop.run_until_complete(get_symbol_precision(symbol))
+    return f"{value:.{precision}f}"
